c89193950ec134ed7f998a26f068471fb182786d
szzd7223 <mohdsaadshaikh17@gmail.com>
2025-10-27 17:46:32 +0530

    03: trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
-const page = () => {
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./Client";
+
+
+
+const page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "SSSS Prefetch"}));
+
   return (
-    <div>
-      Ello
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Client />
+    </HydrationBoundary>
   );
 }
 
