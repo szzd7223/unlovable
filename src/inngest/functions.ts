a3abd4eb5cf13745f00ec65e3fe02457599ed384
szzd7223 <mohdsaadshaikh17@gmail.com>
2025-11-06 02:09:11 +0530

    06: e2b sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,19 @@
+import { Sandbox } from "@e2b/code-interpreter"
+
 import { createAgent, openai } from "@inngest/agent-kit";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-test-2");
+      return sandbox.sandboxId;
+    });
+
     const writer = createAgent({
       name: "writer",
       system: "You are an expert writer.  You write readable, concise, simple content.",
@@ -20,6 +28,12 @@ export const helloWorld = inngest.createFunction(
       `Summarize the following content: ${event.data.value}`,
     );
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`
+    })
+
+    return { output, sandboxUrl };
   },
 );
